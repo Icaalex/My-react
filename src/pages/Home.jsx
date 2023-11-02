5fb1c1f05e141ff48aaa129b3f7c0688d052e121
code-flag <awefrancolaz@gmail.com>
2023-11-02 15:00:01 +0100

    hero section completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,11 @@ import { Button } from './../components/Button';
 
 export const HeroSection = () => {
     return(
-        <div className="heroSection">
+        <div className="heroSection d-flex">
             {/* <div className="image"></div> */}
             <div className="caption d-flex col">
                 <div className="c1">welcome to salvaltion chuurch</div>
-                <div className="c2">perfect church for <br /> imperfect people</div>
+                <div className="c2">perfect church for imperfect people</div>
                 <div className="c3">far far away, behind the world montains, far from te 
                 countries vocaliar and consonatia, there live the blind text. Separated they lived in.</div>
                 <Button btnTxt="register now" />
